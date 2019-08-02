605fe49995ef6c397fb4344d074b0538809589bd
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2019-08-02 21:47:07 +0200

    feat: added dynamicly added repos from github

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 "use strict";
 console.log(`Hello! Nice to see you here`);
 
+const cards = document.querySelector('.githubRepos__list');
 
+fetch('https://api.github.com/users/OrianaKJ/repos')
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        for (const repo of repos) {
+            cards.innerHTML += `<li><a href="${repo.html_url}">${repo.name} </a></li>`;
+        }
+
+    })
+    .catch(err => {
+        console.log(err);
+    })
