07c5d8edce696a788abcc72f8a8855137231ed95
Oriana Kozioryńska-Jurowicz <oriana.koziorynska@gmail.com>
2019-08-09 11:00:16 +0200

    feat: javascript for updating repos from github

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,32 @@
 "use strict";
 console.log(`Hello! Nice to see you here`);
 
-// const cards = document.querySelector('.githubRepos__list');
+const cards = document.querySelector('.githubRepos__list');
 
-// fetch('https://api.github.com/users/OrianaKJ/repos')
-//     .then(resp => resp.json())
-//     .then(resp => {
-//         const repos = resp;
-//         for (const repo of repos) {
-//             cards.innerHTML += `<li><a href="${repo.html_url}">${repo.name} </a></li>`;
-//         }
+fetch('https://api.github.com/users/OrianaKJ/repos')
+    .then(resp => resp.json())
+    .then(resp => {
+        const repos = resp;
+        for (const repo of repos) {
+            const { description, homepage, html_url, name } = repo;
+            console.log(repo);
+            cards.innerHTML += `<li class="project">
+            <article class="project__content">
+              <img src="../assets/img/githubblack.svg" alt="" class="project__icon">
+              <h4 class="project__title">${name}</h4>
+              ${description ? ` <p class="project__description">${description}</p>` : 'No description😒'}
+            </article>
 
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
+            <footer class="project__footer">
+              <article class="project__footer-links">
+              ${homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}.">Demo</a>` : ''}
+                <a class="project__link project__link--github" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}">GitHub</a>
+              </article>
+            </footer>
+          </li>`;
+        }
+
+    })
+    .catch(err => {
+        console.log(err);
+    })
